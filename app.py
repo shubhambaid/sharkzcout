469412db1a88b6a4cc1b45d349a7548f570c5347
shubhambaid <shubhambaid99@gmail.com>
2019-10-02 21:21:55 +0530

    removed unwanted comments

--- app.py
+++ app.py
@@ -5,18 +5,14 @@ jack=[]
 app = Flask(__name__)
 pkts = sniff(count = 20)
 i=0
-#j=0
 for i in range(0,19):
     if  pkts[i][0].src=='MAC OF DEV 1' or pkts[i][0].dst=='MAC OF DEV 1':
         dev1=pkts[i].summary()
-#for j in range(0,9999):            #For Device 2 and so on
-    #if  pkts[j][0].src=='' or pkts[j][0].dst=='':
-        #jack=pkts[j].summary
 @app.route('/')
 def index():
     filter1 = {
         'msisummary':dev1 ,
-        #'macsummary':jack ,
+
     }
 
     return render_template('index.html',filter1=filter1)
